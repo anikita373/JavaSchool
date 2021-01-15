8b2870cbf37bc150cca1078a8160da78d665f77b
anikita373 <68057401+anikita373@users.noreply.github.com>
2021-01-16 02:22:54 +0300

    Add files via upload

---
+++ Maven-done/task1/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,14 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+		ВЫ ДОПУСТИЛИ ПОТЕРЮ ДОРОГОСТОЯЩЕГО ОБМУНДИРОВАНИЯ!
+        System.out.println( "Hello World!" );
+    }
+}
